afed1f8a79c793a90d8ced57ec2325f50180d3ec
blacklead <blacklead@users.noreply.github.com>
2014-06-18 20:00:06 +0900

    Create f08.fs

---
+++ f08.fs
@@ -0,0 +1,8 @@
+let rec compress l =
+  match l with
+    | [] -> []
+    | x::y::xs when x = y -> compress(y::xs)
+    | x::xs -> x::compress xs
+;;
+
+compress ['a';'a';'a';'a';'b';'c';'c';'a';'a';'d';'e';'e';'e';'e'];;
